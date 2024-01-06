4af839c7c7400d8d1e57282165eee734db030bc7
Liam Dyer <liam@Liams-Mac-mini.local>
2024-01-05 19:34:30 -0600

    Add tracing to server

--- cm-server/src/main.rs
+++ cm-server/src/main.rs
@@ -2,6 +2,9 @@ use anyhow::Result;
 use prost::{decode_length_delimiter, length_delimiter_len};
 use quinn::Endpoint;
 use std::{error::Error, net::SocketAddr, sync::Arc};
+use tracing::{error, info, info_span, trace, trace_span};
+use tracing_futures::Instrument;
+use tracing_subscriber;
 
 use cm_protos::{
     cm_proto::messages::{circle_mover_message::Value, CircleMoverMessage},
@@ -23,10 +26,14 @@ fn generate_self_signed_cert() -> Result<(rustls::Certificate, rustls::PrivateKe
 
 #[tokio::main]
 async fn main() {
-    println!("Starting server!");
+    tracing::subscriber::set_global_default(tracing_subscriber::FmtSubscriber::builder().finish())
+        .unwrap();
+
+    info!("Starting server!");
     server().await.unwrap();
 }
 
+#[tracing::instrument]
 async fn server() -> Result<(), Box<dyn Error>> {
     let (cert, key_der) = generate_self_signed_cert()?;
     let server_crypto = rustls::ServerConfig::builder()
@@ -37,11 +44,14 @@ async fn server() -> Result<(), Box<dyn Error>> {
 
     let endpoint = Endpoint::server(server_config, server_addr())?;
 
-    // Start iterating over incoming connections.
+    trace!("Accepting connections");
     while let Some(conn) = endpoint.accept().await {
-        let connection_fut = handle_connection(conn);
         // Spawn a task for each connection
-        tokio::spawn(connection_fut);
+        tokio::spawn(async {
+            if let Err(e) = handle_connection(conn).await {
+                error!("Failed: {reason}", reason = e.to_string())
+            }
+        });
     }
 
     Ok(())
@@ -49,64 +59,78 @@ async fn server() -> Result<(), Box<dyn Error>> {
 
 async fn handle_connection(conn: quinn::Connecting) -> Result<()> {
     let connection = conn.await?;
+    let span = info_span!(
+        "connection",
+        remote = %connection.remote_address(),
+        protocol = %connection
+            .handshake_data()
+            .unwrap()
+            .downcast::<quinn::crypto::rustls::HandshakeData>().unwrap()
+            .protocol
+            .map_or_else(|| "<none>".into(), |x| String::from_utf8_lossy(&x).into_owned())
+    );
+    info!("Connection to client");
 
-    println!("waiting to accept bi");
-    let stream = connection.accept_bi().await;
-    let (mut _send, mut recv) = match stream {
-        Err(quinn::ConnectionError::ApplicationClosed { .. }) => {
-            println!("Connection Error");
-            return Ok(());
-        }
-        Err(e) => {
-            println!("{:?}", e);
-            return Err(e.into());
-        }
-        Ok(s) => s,
-    };
-    println!("Bi accepted");
-
-    loop {
-        // FIXME: This will bug out if receiving a message < 9 bytes
-        // Length delimeter is at most 10 bytes
-        let mut len_delimeter_buf = [0u8; 10];
-        println!("Waiting for 10 bytes");
-        recv.read_exact(&mut len_delimeter_buf).await?;
-        println!("Got 10 bytes");
-        // Use the 10 bytes to determine the length of the next message
-        let len_delimeter = decode_length_delimiter(&len_delimeter_buf[..])?;
-        // Determine how big the length delimeter is, we now need to read len_delimeter - (10 - delim_len) more bytes
-        // to get our message
-        let delim_len = length_delimiter_len(len_delimeter);
-
-        let rest_len = len_delimeter - (10 - delim_len);
-
-        println!("Waiting for {} more bytes", rest_len);
-        let mut rest_buf = vec![0u8; len_delimeter - (10 - delim_len)];
-        recv.read_exact(&mut rest_buf).await?;
-        println!("Got em");
-
-        let msg_buf = [&len_delimeter_buf[delim_len..], &rest_buf[..]].concat();
-        println!("Message buf: {:?}", msg_buf);
-
-        match deserialize_message(&msg_buf) {
-            Ok(msg) => match msg {
-                CircleMoverMessage {
-                    value: Some(Value::Hello(hello_msg)),
-                } => {
-                    println!("Hello {}", hello_msg.name)
-                }
-                CircleMoverMessage {
-                    value: Some(Value::Goodbye(goodbye_msg)),
-                } => {
-                    println!("Goodbye {}", goodbye_msg.name)
-                }
-                CircleMoverMessage { value: None } => {
-                    println!("Empty value")
-                }
-            },
+    async {
+        trace!("Awaiting bidirectional connection");
+        let stream = connection.accept_bi().await;
+        let (mut _send, mut recv) = match stream {
+            Err(quinn::ConnectionError::ApplicationClosed { .. }) => {
+                info!("Connection closed");
+                return Ok(());
+            }
             Err(e) => {
-                println!("{:?}", e)
+                return Err(e.into());
+            }
+            Ok(s) => s,
+        };
+        trace!("Bidirectional connection established");
+
+        loop {
+            let span = trace_span!("Reading message");
+            let _enter = span.enter();
+            // FIXME: This will bug out if receiving a message < 9 bytes
+            // Length delimeter is at most 10 bytes
+            let mut len_delimeter_buf = [0u8; 10];
+
+            trace!("Waiting for 10 bytes");
+            recv.read_exact(&mut len_delimeter_buf).await?;
+            trace!("Received 10 bytes from client");
+            // Use the 10 bytes to determine the length of the next message
+            let len_delimeter = decode_length_delimiter(&len_delimeter_buf[..])?;
+            // Determine how big the length delimeter is, we now need to read len_delimeter - (10 - delim_len) more bytes
+            // to get our message
+            let delim_len = length_delimiter_len(len_delimeter);
+
+            let rest_len = len_delimeter - (10 - delim_len);
+
+            trace!("Waiting for more bytes to complete proto message");
+            let mut rest_buf = vec![0u8; rest_len];
+            recv.read_exact(&mut rest_buf).await?;
+            trace!("Received all necessary bites");
+
+            let msg_buf = [&len_delimeter_buf[delim_len..], &rest_buf[..]].concat();
+            trace!("Message buffer constructed {:?}", msg_buf);
+
+            match deserialize_message(&msg_buf) {
+                Ok(msg) => {
+                    info!("Received message: {:?}", msg);
+                    match msg {
+                        CircleMoverMessage {
+                            value: Some(Value::Hello(_hello_msg)),
+                        } => {}
+                        CircleMoverMessage {
+                            value: Some(Value::Goodbye(_goodbye_msg)),
+                        } => {}
+                        CircleMoverMessage { value: None } => {}
+                    }
+                }
+                Err(e) => {
+                    error!("Proto decode error: {:?}", e)
+                }
             }
         }
     }
+    .instrument(span)
+    .await
 }
